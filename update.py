f882674477d4dc585d5733a1aae723b3db55fafc
Yuval Adam <_@yuv.al>
2016-09-17 12:54:09 +0300

    Add coindesk rate

--- update.py
+++ update.py
@@ -1,3 +1,14 @@
+import requests
+
 from app import db, Transaction
 
-print('done')
+def get_coindesk_rate():
+    TICKER_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    ticker = requests.get(TICKER_URL)
+    rate = ticker.json()['bpi']['USD']['rate_float']
+    return rate
+
+print('Starting update.py')
+
+print('Current coindesk BTC/USD rate: ${}'.format(get_coindesk_rate()))
+
