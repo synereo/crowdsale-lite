f500d503508238539f019012656c256850f85edf
Yuval Adam <_@yuv.al>
2016-09-17 12:44:42 +0300

    Add templates and cleanup app

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 
 from flask import Flask
 from flask import render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 
@@ -35,13 +35,6 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/robots.txt')
-def robots():
-    res = app.make_response('User-agent: *\nAllow: /')
-    res.mimetype = 'text/plain'
-    return res
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-
-app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
